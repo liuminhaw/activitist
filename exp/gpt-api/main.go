18c67077da77e52bf61911cd0e36a0104e263ecb
Min-Haw, Liu <liuminhaw@gmail.com>
2023-08-27 23:42:04 +0800

    feat: linebot webhook and activity assistant minimal function

---
+++ exp/gpt-api/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/sashabaranov/go-openai/jsonschema"
+)
+
+const gptKey = "GPT Key here"
+const prompt = ""
+
+const systemRole = `You are an professional traditional chinese activity assistant, please analyze given input and conclude in points below:
+- action: should be one of "create" if aimed to create an activity, "update" if aimed to make change to activity, "delete" if aimed to remove an activity, "list" if aim to view activities, or "undefined" if non of above is matched
+- name: name of the activity, should be easy to recognize and non-repetitive
+- starttime (in the format of YYYY-mm-dd HH:MM:SS)
+- endtime (in the format of YYYY-mm-dd HH:MM:SS)
+- location
+Items other than action and name can be omitted if there is no information.Based on the provided information, please determine whether this is intended for adding an event, updating an event, deleting an event, listing events, or if it's indeterminate. Fill in the action field with "create", "update", "delete", "list", or "undefined" accordingly.
+For the name field, provide a distinctive event name that is not easily confused with other events in traditional Chinese.
+For the starttime field, provide the event's starting time.
+For the endtime field, provide the event's ending time, leaving it empty if no specific information is available.
+For the location field, provide the event's location.
+Except for action key, which are mandatory, the other fields should not be included if there is no available information to determine their content.
+For example, if the message is "Add location to the Thin Eatery event: Thin Eatery," it should be interpreted as "Modify the location of the Thin Eatery event to Thin Eatery."
+Here are possible JSON responses:
+Adding an event: {"action": "create","name": "Festival Concert","starttime": "2023-09-15 18:00","endtime": "2023-09-15 22:00","location": "City Center Music Hall"}
+Deleting an event: {"action": "delete","name": "Weekend Market"}
+Updating an event: {"action": "update","name": "Health Seminar","starttime": "2023-09-20 14:00","endtime": "2023-09-20 16:00","location": "Health Center"}
+Listing events: {"action": "list"}
+Indeterminate situation: {"action": "undefined"}`
+
+var schema openai.FunctionDefinition = openai.FunctionDefinition{
+	Name: "get_action",
+	Parameters: jsonschema.Definition{
+		Type: jsonschema.Object,
+		Properties: map[string]jsonschema.Definition{
+			"action": {
+				Type:        jsonschema.String,
+				Description: "Action aimed to produced on given activity",
+				Enum:        []string{"create", "update", "delete", "list", "undefined"},
+			},
+			"name": {
+				Type:        jsonschema.String,
+				Description: "Name of the activity, should be easy to recognize and non-repetitive",
+			},
+			"starttime": {
+				Type:        jsonschema.String,
+				Description: "Start time of the activity, should be in ISO 8601 format",
+			},
+			"endtime": {
+				Type:        jsonschema.String,
+				Description: "End time of the activity, should be in ISO 8601 format",
+			},
+			"location": {
+				Type:        jsonschema.String,
+				Description: "Location of the activity",
+			},
+		},
+		Required: []string{"action", "name"},
+	},
+}
+
+func main() {
+	client := openai.NewClient(gptKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: systemRole,
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+			Functions:    []openai.FunctionDefinition{schema},
+			FunctionCall: &openai.FunctionCall{Name: "get_action", Arguments: ""},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	// fmt.Println(resp.Choices[0].Message.FunctionCall.Arguments)
+}
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	"github.com/liuminhaw/activitist/gpt"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	viper.SetConfigName("activitist")         // name of config file (without extension)
+	viper.SetConfigType("yaml")               // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/activitist.d/") // path to look for the config file in
+	viper.AddConfigPath(".")                  // optionally look for config in the working directory
+	err := viper.ReadInConfig()               // Find and read the config file
+	if err != nil {                           // Handle errors reading the config file
+		log.WithFields(log.Fields{"event": "readConfig"}).Fatal(err)
+	}
+
+	channelSecret := viper.GetString("line.channelSecret")
+	channelToken := viper.GetString("line.channelToken")
+	// log.Debugf("Line channel secret: %s", channelSecret)
+	// log.Debugf("Line channel token: %s", channelToken)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to activitist!!!"))
+	})
+	r.Post("/line/message", func(w http.ResponseWriter, r *http.Request) {
+		bot, err := linebot.New(channelSecret, channelToken)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"method": "POST",
+				"path":   "/line/message",
+			}).Errorf("new linebot: %w", err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+		events, err := bot.ParseRequest(r)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"method": "POST",
+				"path":   "/line/message",
+			}).Errorf("parse request: %w", err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					log.WithFields(log.Fields{
+						"method": "POST",
+						"path":   "/line/message",
+						"event":  "textMessage",
+					}).Info(message.Text)
+					if ok := strings.HasPrefix(message.Text, "@act"); ok {
+						prompt := strings.ReplaceAll(message.Text, "@act", "")
+						gptService := gpt.NewAssistantService(&gpt.AssistantConfig{
+							Key: viper.GetString("gptApi.key"),
+						}, prompt)
+						replyMessage, err := gptService.AnalyzeMessage()
+						if err != nil {
+							log.WithFields(log.Fields{
+								"method": "POST",
+								"path":   "/line/message",
+								"event":  "textMessage",
+							}).Error(err)
+							return
+						}
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
+					}
+				}
+			}
+		}
+	})
+	http.ListenAndServe(":3000", r)
+}
